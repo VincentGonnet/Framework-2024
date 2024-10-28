b685735a62da32bf6183a05da543150a3f2fcf33
Draily Esteban <esteban.draily@etu.univ-orleans.fr>
2024-10-28 19:52:41 +0100

    added an "about/" url, view and template

--- cc/collec_management/views.py
+++ cc/collec_management/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    context = {"message" : "Hello, World!"}
+    return render(request, 'collec_management/about.html',context)
